fb45b616293dbd1e1b0acb7c6a520589cf48fc74
Noam Seifer <noamseifer@gmail.com>
2025-06-02 12:21:54 +0300

    emails feature added

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,15 @@
-from app import add
+from app import add, check_is_email
 
 def test_add():
     assert add(2, 3) == 5
+
+
+def test_check_is_email():
+    assert check_is_email("abc@gmail.com") == True
+    assert check_is_email("user.name+tag+sorting@example.com") == True
+    assert check_is_email("plainaddress") == False
+    assert check_is_email("missin_at_sign.com") == False
+    assert check_is_email("missingdomain@.com") == False
+    assert check_is_email("") == False
+
+
