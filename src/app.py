f58e0c9ebe4eb64ed94ec56939c693872887b6b2
Noam Seifer <noamseifer@gmail.com>
2025-06-02 12:33:49 +0300

    fixed linting + added redis to requirements.txt

--- src/app.py
+++ src/app.py
@@ -2,41 +2,53 @@ from flask import Flask, request, render_template
 import redis
 import re
 
-app = Flask(__name__)   
+app = Flask(__name__)
 
-# redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True) 
-redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True) 
+# Will need to add redis to our multi-container
+# redis_client = 
+# redis.Redis(host='redis', port=6379, db=0, decode_responses=True) 
+redis_client = redis.Redis(
+    host='localhost',
+    port=6379,
+    db=0,
+    decode_responses=True
+)
 
 
 # Basic email validation:
 # - Requires one or more non-whitespace characters before the @
 # - Requires one or more non-whitespace characters after the @ and before the .
 # - Requires one or more non-whitespace characters after the .
-email_validate_pattern = r"^\S+@\S+\.\S+$"  
+email_validate_pattern = r"^\S+@\S+\.\S+$"
 
 
 @app.route("/", methods=["GET", "POST"])
 def main_page():
     result = None
-    if request.method == "POST":        
+    if request.method == "POST":
         email = str(request.form["userEmail"])
         if check_is_email(email):
             if redis_client.sismember("emails-set", email):
                 result = "Email already registered"
             else:
-                result = "Valid Email"            
+                result = "Valid Email"
                 redis_client.sadd("emails-set", email)
         else:
-            result = "Invalid Email"            
+            result = "Invalid Email"
     return render_template("index.html", result=result)
 
 
 @app.route("/emails")
 def emails_page():
-    emails =  redis_client.smembers('emails-set')
+    emails = redis_client.smembers('emails-set')
     emails = sorted(emails)
     cardinality = redis_client.scard("emails-set")
-    return render_template("emails.html", emails= emails, cardinality=cardinality)
+    
+    return render_template(
+        "emails.html",
+        emails= emails,
+        cardinality=cardinality
+    )
 
 
 def check_is_email(i_InputString) -> bool:
