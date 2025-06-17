bdbec29533f9ac429f18726616fa7c21e67ff669
omer droub <omerdr2@gmail.com>
2025-06-17 13:59:09 +0300

    i fixed the redis implementation in compose and app

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ emails_added_total = Counter(
 # redis_client =
 # redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 redis_client = redis.Redis(
-    host='localhost',
+    host='redis_host',
     port=6379,
     db=0,
     decode_responses=True
