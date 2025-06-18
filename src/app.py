17bb95d4773d46c188cbed869b49931024f492a8
Noam Seifer <noamseifer@gmail.com>
2025-06-18 17:51:55 +0300

    scraping from app works for emails_added_total

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
 import redis
 import re
-from prometheus_client import Counter
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
 
 app = Flask(__name__)
 
@@ -62,7 +63,7 @@ def emails_page():
 
 @app.route("/metrics")
 def metrics():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+    return generate_latest(), 200, { 'Content-Type':CONTENT_TYPE_LATEST }
 
 
 def check_is_email(i_InputString) -> bool:
